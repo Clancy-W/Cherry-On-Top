79c108b2e783db2a5806fd912268af6ac499845a
Clancy-W <34616988+Clancy-W@users.noreply.github.com>
2018-10-23 09:35:12 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: `
+			<h1>Cherry on Top</h1>
+			<h1>Ice Cream Shop</h1>
+			<p>Cherry on Top Ice Cream Shop is a startup ice cream business which delivers ice cream to your door. We offer ice cream designed by you, made by us. To find our menu, go to the <a href="#menu">Menu Page</a>, which has our full list of ingredients for you to order, for more information about us, go to the <a href="#about">About Page</a>, and to find out how to contact us go to the <a href="#contact">Contact Page</a></p>
+			`,
+		hash: window.location.hash
     }
 });
