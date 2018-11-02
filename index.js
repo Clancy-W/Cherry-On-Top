646ae18fa129f7a66fff86791f00be7f7c521a40
Clancy-W <34616988+Clancy-W@users.noreply.github.com>
2018-11-02 11:48:35 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ var app = new Vue({
 						  <a href="#">Go to the Store</a>
 						</div>
 					  </div>
-					</div>
+				</div>
+				
 				</div>
 			  </div></div>
 			<h1>Social Media</h1>
 			<div class="container"><h3 align="center">Find us on our social media platforms</h3><p>Our business sees the future is completely online, so we promote having no store front, and going fully online. Do you really want to leave the house to get ice cream? You do it with pizza, its about time you did it with ice cream. You can also find us on many different sites, so you can get updated easily.</p>
 			<div class="row">
-					<div class="col s3" align="center">
+					<div class="col s12 m6 l3" align="center">
 						<div class="card hoverable">
 							<div class="card-content">
 							  <img src="https://cdn2.iconfinder.com/data/icons/free-social-media-17/155/facebook-512.png" style="width:50%">
@@ -35,7 +36,7 @@ var app = new Vue({
         </div>
 						</div>
 					</div>
-					<div class="col s3" align="center">
+					<div class="col s12 m6 l3 full" align="center">
 						<div class="card hoverable">
 							<div class="card-content">
 							  <img src="https://cdn2.iconfinder.com/data/icons/free-social-media-17/158/twitter-512.png" style="width:50%">
@@ -47,19 +48,19 @@ var app = new Vue({
         </div>
 						</div>
 					</div>
-					<div class="col s3" align="center">
+					<div class="col s12 m6 l3" align="center">
 						<div class="card hoverable">
 							<div class="card-content">
 							  <img src="https://cdn2.iconfinder.com/data/icons/free-social-media-17/157/youtube_2-512.png" style="width:50%">
 							  <span class="card-title">YouTube</span>
-							  <p>On our YouTube page you will get sneak peeks on new flavours, cones and more!</p>
+							  <p>On our YouTube page you will be able to peek at new flavours, cones and more!</p>
 							</div>
 <div class="card-action">
           <a href="http://www.youtube.com">Take me there!</a>
         </div>
 						</div>
 					</div>
-					<div class="col s3" align="center">
+					<div class="col s12 m6 l3" align="center">
 						<div class="card hoverable">
 							<div class="card-content">
 							  <img src="https://cdn2.iconfinder.com/data/icons/free-social-media-17/155/instagram-512.png" style="width:50%">
@@ -72,6 +73,59 @@ var app = new Vue({
 						</div>
 					</div>
 				</div></div>
+				<div class="container"><div class="section"><div class="row"><div class="col s12 l6">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            <div class="col s2">
+              <img src="http://sg-fs.com/wp-content/uploads/2017/08/user-placeholder.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div><div class="col s12 l6">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+            <div class="col s2">
+              <img src="http://sg-fs.com/wp-content/uploads/2017/08/user-placeholder.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+          </div>
+        </div>
+      </div><div class="col s12 l6">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            <div class="col s2">
+              <img src="http://sg-fs.com/wp-content/uploads/2017/08/user-placeholder.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div><div class="col s12 l6">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+            <div class="col s2">
+              <img src="http://sg-fs.com/wp-content/uploads/2017/08/user-placeholder.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+          </div>
+        </div>
+      </div></div></div></div>
 			`,
 		menu: `<h1>Menu</h1>
 				<p>At <em>Cherry on Top Ice Cream</em>, we pride ourselves on our variance of flavours suited for everyones' tastebuds. We offer many different types of ice cream and sorbets, you can also mix in your own items, like chocolate. Here is a list of some of our flavours.
